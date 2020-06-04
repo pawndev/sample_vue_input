6b6c20d8a74164f9ae1b687614a76b2aa5cd9a2a
Pawndev <coquelet.c@gmail.com>
2020-06-04 10:32:19 +0200

    feat(setup): Initial commit

---
+++ components/BasicForm.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-full max-w-xs">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <slot></slot>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ components/BasicInput.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="mb-4">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+      Courriel
+    </label>
+    <input
+      id="email"
+      v-model="userData.email"
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      type="text"
+      placeholder="E-mail"
+      @blur="saveData"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userData: {
+        email: ''
+      }
+    }
+  },
+  mounted() {
+    console.log({ ...localStorage })
+    if (localStorage.email) {
+      this.userData.email = localStorage.email
+    }
+  },
+  methods: {
+    saveData(evt) {
+      localStorage.email = this.userData.email
+    }
+  }
+}
+</script>
